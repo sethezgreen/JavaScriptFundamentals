92c0488927cde3a3328cbcb9b30edad55d22d8e6
Seth Green <sethezgreen@gmail.com>
2023-12-06 22:06:50 -0500

    first commit

---
+++ hook-form/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import UserForm from './components/userForm'
+
+function App() {
+  return (
+    <>
+      <UserForm/>
+    </>
+  )
+}
+
+export default App
---
+++ hook-form/src/components/UserForm.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+
+const UserForm = (props) => {
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
+
+    const createUser = (e) => {
+        e.preventDefault()
+
+        const newUser = {firstName, lastName, email, password, confirmPassword}
+        setFirstName("")
+        setLastName("")
+        setEmail("")
+        setPassword("")
+        setConfirmPassword("")
+    }
+
+    return (
+        <>
+            <form onSubmit={createUser}>
+                <div>
+                    <label>First Name:</label>
+                    <input type="text" value={firstName} onChange={ (e) => setFirstName(e.target.value)}/>
+                </div>
+                <div>
+                    <label>Last Name:</label>
+                    <input type="text" value={lastName} onChange={ (e) => setLastName(e.target.value)}/>
+                </div>
+                <div>
+                    <label>Email:</label>
+                    <input type="text" value={email} onChange={ (e) => setEmail(e.target.value)}/>
+                </div>
+                <div>
+                    <label>Password:</label>
+                    <input type="text" value={password} onChange={ (e) => setPassword(e.target.value)}/>
+                </div>
+                <div>
+                    <label>Confirm Password:</label>
+                    <input type="text" value={confirmPassword} onChange={ (e) => setConfirmPassword(e.target.value)}/>
+                </div>
+                <div>
+                    <input type="submit" value="Submit" />
+                </div>
+            </form>
+
+            <div>
+                <p>Your Form Data</p>
+                <p>First Name: {firstName}</p>
+                <p>Last Name: {lastName}</p>
+                <p>Email: {email}</p>
+                <p>Password: {password}</p>
+                <p>Confirm Password: {confirmPassword}</p>
+            </div>
+        </>
+    )
+}
+
+export default UserForm
