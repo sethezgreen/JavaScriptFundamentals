ec1e2436a4cba3fd8ae0ef4339ef880a115cf777
Seth Green <sethezgreen@gmail.com>
2023-12-07 22:59:31 -0500

    started validations

--- hook-form/src/components/UserForm.jsx
+++ hook-form/src/components/UserForm.jsx
@@ -6,6 +6,11 @@ const UserForm = (props) => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [confirmPassword, setConfirmPassword] = useState("")
+    const [firstNameError, setFirstNameError] = useState("")
+    const [lastNameError, setLastNameError] = useState("")
+    const [emailError, setEmailError] = useState("")
+    const [passwordError, setPasswordError] = useState("")
+    const [confirmPasswordError, setConfirmPasswordError] = useState("")
 
     const createUser = (e) => {
         e.preventDefault()
@@ -18,12 +23,24 @@ const UserForm = (props) => {
         setConfirmPassword("")
     }
 
+    const handleFirstName = (e) => {
+        setFirstName(e.target.value)
+        if(e.target.value.length < 1) {
+            setFirstNameError("First Name must not be blank!")
+        } else if(e.target.value.length < 2) {
+            setFirstNameError("First Name must be at least 2 characters")
+        } else {
+            setFirstNameError("")
+        }
+    }
+
     return (
         <>
             <form onSubmit={createUser}>
                 <div>
                     <label>First Name:</label>
                     <input type="text" value={firstName} onChange={ (e) => setFirstName(e.target.value)}/>
+                    { firstNameError ? <p>{firstNameError}</p> : null}
                 </div>
                 <div>
                     <label>Last Name:</label>
@@ -42,7 +59,11 @@ const UserForm = (props) => {
                     <input type="text" value={confirmPassword} onChange={ (e) => setConfirmPassword(e.target.value)}/>
                 </div>
                 <div>
-                    <input type="submit" value="Submit" />
+                    {
+                        firstNameError ?
+                        <input type="submit" value="Submit" disabled/> :
+                        <input type="submit" value="Submit" />
+                    }
                 </div>
             </form>
 
