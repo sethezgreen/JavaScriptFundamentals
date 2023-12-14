621ab8a8d662f885f4b12f49573e4238f41fca7e
Seth Green <sethezgreen@gmail.com>
2023-12-13 19:29:37 -0500

    added box color functionality

--- box-generator/src/components/BoxDisplay.jsx
+++ box-generator/src/components/BoxDisplay.jsx
@@ -3,11 +3,10 @@ import React, { useState } from "react";
 const BoxDisplay = (props) => {
     const {boxes, setBoxes} = props
     return (
-        <div>
+        <div style={{display: "inline-grid"}}>
             {
                 boxes.map((box) => (
-                    <div>
-                        <p>{box.color}</p>
+                    <div style={{backgroundColor: box.color, width: 100, height: 100, margin: 15}}>
                     </div>
                 ))
             }
