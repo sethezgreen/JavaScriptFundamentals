0026262c648b7511384f5eda4687fba4ce72d785
Seth Green <sethezgreen@gmail.com>
2023-12-13 20:39:06 -0500

    fixed react key error

--- box-generator/src/components/BoxDisplay.jsx
+++ box-generator/src/components/BoxDisplay.jsx
@@ -5,8 +5,8 @@ const BoxDisplay = (props) => {
     return (
         <div style={{display: "inline-grid"}}>
             {
-                boxes.map((box) => (
-                    <div style={{backgroundColor: box.color, width: 100, height: 100, margin: 15}}>
+                boxes.map((box, idx) => (
+                    <div key={idx} style={{backgroundColor: box.color, width: 100, height: 100, margin: 15}}>
                     </div>
                 ))
             }
